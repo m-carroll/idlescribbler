eb9395ed57f82c4353e18ccc2bc68752fa918e04
Kelly Oh <Kelly@Kelly-Ohs-MBP.lan>
2020-02-16 15:00:01 -0500

    style search bar top pf pg

--- js/responsiveUtils.js
+++ js/responsiveUtils.js
@@ -2,8 +2,14 @@
 function makeTopnavResponsive() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-        x.className += " responsive";
+        x.className = "topnav responsive";
     } else {
         x.className = "topnav";
     }
+    var y = document.getElementById("cover");
+    if (y.className === "nonresponsivecover") {
+        y.className = "responsivecover";
+    } else {
+        y.className = "nonresponsivecover";
+    }    
 }
